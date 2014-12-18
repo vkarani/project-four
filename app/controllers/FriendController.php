2d522cd985b0d381ce4036944b491bdb6372f712
Victor Karani <victor.karani@gmail.com>
2014-12-18 12:21:10 -0500

    Can add friend with some checking

--- app/controllers/FriendController.php
+++ app/controllers/FriendController.php
@@ -86,33 +86,27 @@ class FriendController extends BaseController {
     
     if ($validator->passes()) {
     	//get id from email address...
-    	$friend_users= User::where('email','=' ,'user4@email.com') -> get();
+    	$friend_users= User::where('email','=' , Input::get('friend') ) -> get();
     	//echo Paste\Pre::render($friend_users); //DEBUG
     	
-    	//Do a check for size
-    	foreach($friend_users as $friend_user){
-    		echo Paste\Pre::render($friend_user -> id); //DEBUG
-    		$current_user = Auth::user();
-    		$current_user->add_friend($friend_user ->id);
-    		return Redirect::to('/friends')->with('flash_message', 'Friend '.$friend_user -> email.' added');
+    	$result = count($friend_users);
+    	//echo Paste\Pre::render($result); //DEBUG
+    	
+    	if ($result > 0){
+    		foreach($friend_users as $friend_user){
+    		  //echo Paste\Pre::render($friend_user -> id); //DEBUG
+    		  $current_user = Auth::user();
+    		  $current_user->add_friend($friend_user ->id);
+    		  return Redirect::to('/friends')->with('flash_message', 'Friend '.$friend_user -> email.' added');
+    	  }
     	}
-    	
-    	//TODO add fail case.
-    	
-    	/*
-    	try {
-	      //$friend = User::findOrFail(Input::get('friend'));
-	      $friend = User::findOrFail('1');
-	      echo Paste\Pre::render($friend); //DEBUG
-	    }
-	    catch(exception $e) {
-	      return Redirect::to('/friends')->with('flash_message', 'The selected User does not exist');
-	    }
-	    */
+    	else{
+    		return Redirect::to('/friends')->with('flash_message', 'The selected User does not exist');
+    	}
     
     }
 
   }
-  
+
   
 }
